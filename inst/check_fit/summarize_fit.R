78e708df6f90532bc99be34eb37ff73a3c9c25cf
Thomas Rosendal <thomas.rosendal@sva.se>
2023-03-08 16:16:45 +0100

    Modelfit plot

--- inst/check_fit/summarize_fit.R
+++ inst/check_fit/summarize_fit.R
@@ -9,7 +9,9 @@ ex <- ex[index]
 
 ## Now to just get back to the original scale of the distance the I'll
 ## multiply the fit$error results by the expected values. The result
-## is then the crude difference in prevalance.
+## is then the crude difference in prevalance. Where a positive value
+## is means the Broens values are higher than the model and negative
+## values means the model is higher than Broens.
 
 sum_fit <- lapply(fit, function(x) {
     lapply(seq_len(length(x$error)), function(y) {
@@ -22,7 +24,21 @@ sum_fit <- lapply(seq_len(length(sum_fit[[1]])), function(x) {
 })
 names(sum_fit) <- index
 
-par(mfrow = c(2, 4))
+titles <- c(q_m1s = "Sows when they enter the farrowing unit",
+            q_m2s = "sows three days after entering the farrowing unit",
+            q_m2p = "Piglets three days after entering the farrowing unit",
+            q_m3s = "Sows three weeks after entering the farrowing unit",
+            q_m3p = "Piglets three weeks after entering the farrowing unit",
+            q_m4p = "Pigs 7 days after weaning",
+            q_m5p = "Pigs 35 days after weaning",
+            q_m6p = "Pigs 84 days after moving the the finishing unit")
+
+pdf("modelfit.pdf", width = 12, height = 16)
+par(mfrow = c(4, 2))
 for (i in seq_len(length(sum_fit))) {
-    hist(sum_fit[[i]], breaks = 50)
+    ## Swap the sign of sum_fit to get Model result - expected from Broens
+    plot(density(-sum_fit[[i]]),
+         main = titles[names(sum_fit)[i]],
+         xlab = "Model result - expected prevalence")
 }
+dev.off()
